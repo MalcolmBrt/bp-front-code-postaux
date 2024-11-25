9e8dce301ff91fc5236f8fa419433e90992465a6
mbertaina <bertainamalcolm@gmail.com>
2024-11-25 16:06:22 +1100

    feat(CodesPostaux): connexion à l'api backend

--- src/app/codes-postaux/codes-postaux.component.ts
+++ src/app/codes-postaux/codes-postaux.component.ts
@@ -1,17 +1,20 @@
-import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { Validators } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
+import { CodePostauxService } from '../code-postaux.service';
+import { CodePostal } from '../code-postal';
 
 @Component({
   selector: 'app-codes-postaux',
   standalone: true,
   imports: [
     ReactiveFormsModule,
+    JsonPipe,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
@@ -24,9 +27,17 @@ export class CodesPostauxComponent {
   readonly formGroupCommune = new FormGroup({
     nomCommune: new FormControl('', [Validators.required]),
   });
+  private codePostauxService = inject(CodePostauxService);
+  public codesPostauxJson: CodePostal[] = [];
 
   search(): void {
-    const nonCommune = this.formGroupCommune.value;
-    console.log(nonCommune);
+    const nomCommune = this.formGroupCommune.value.nomCommune!; // est forcément non null
+    this.codePostauxService.getCodesPostaux(nomCommune).subscribe({
+      next: (data) => (this.codesPostauxJson = data),
+      error: (err) => {
+        this.codesPostauxJson = [];
+        console.error(err);
+      },
+    });
   }
 }
