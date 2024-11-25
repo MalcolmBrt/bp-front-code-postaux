b880a1c9d8fc9510035972e72bacd30b2bbe2f8d
mbertaina <bertainamalcolm@gmail.com>
2024-11-25 13:15:41 +1100

    feat(CodesPostaux): implémentation formulaire réactif

--- src/app/codes-postaux/codes-postaux.component.ts
+++ src/app/codes-postaux/codes-postaux.component.ts
@@ -1,12 +1,32 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { Validators } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-codes-postaux',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './codes-postaux.component.html',
-  styleUrl: './codes-postaux.component.css'
+  styleUrl: './codes-postaux.component.css',
 })
 export class CodesPostauxComponent {
+  readonly formGroupCommune = new FormGroup({
+    nomCommune: new FormControl('', [Validators.required]),
+  });
 
+  search(): void {
+    const nonCommune = this.formGroupCommune.value;
+    console.log(nonCommune);
+  }
 }
