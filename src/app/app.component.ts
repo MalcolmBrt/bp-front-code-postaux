a04a20a1220a89bae89ebcec5f3da92ede12957c
mbertaina <bertainamalcolm@gmail.com>
2024-12-02 10:04:04 +1100

    feat: ajout sélection type de recherche

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { CodesPostauxComponent } from '../codes-postaux/codes-postaux.component';
 import { BoitesPostalesComponent } from '../boites-postales/boites-postales.component';
+import { MatRadioModule } from '@angular/material/radio';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CodesPostauxComponent, BoitesPostalesComponent],
+  imports: [CodesPostauxComponent, BoitesPostalesComponent, MatRadioModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'bp-front-code-postaux';
+  choixRecherche = 'CodePostal';
 }
