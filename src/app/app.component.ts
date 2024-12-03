0c26dbfc3c7eac4ed9327c89b315a12fe43fc4aa
mbertaina <bertainamalcolm@gmail.com>
2024-12-03 11:11:57 +1100

    feat(BP-1726): implémentation d'une navbar simple

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,12 @@ import { CodesPostauxComponent } from '../codes-postaux/codes-postaux.component'
 import { BoitesPostalesComponent } from '../boites-postales/boites-postales.component';
 import { MatRadioModule } from '@angular/material/radio';
 import { FormsModule } from '@angular/forms';
-
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CodesPostauxComponent, BoitesPostalesComponent, MatRadioModule, FormsModule],
+  imports: [CodesPostauxComponent, BoitesPostalesComponent, MatRadioModule, FormsModule, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
