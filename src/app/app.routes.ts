20f27d4465cd27d342c871262dda3e06d4abc5a6
mbertaina <bertainamalcolm@gmail.com>
2024-12-03 13:56:49 +1100

    feat(BP-1726): mise en place du routage pour les versions

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { FaqComponent } from '../faq/faq.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'v1', pathMatch: 'full' },
+  { path: 'v1', component: AppComponent },
+  { path: 'v2', component: AppComponent },
+  { path: 'faq', component: FaqComponent },
+];
--- src/navbar/navbar.component.ts
+++ src/navbar/navbar.component.ts
@@ -1,17 +1,14 @@
-import {Component} from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
-
-}
-
+export class NavbarComponent {}
