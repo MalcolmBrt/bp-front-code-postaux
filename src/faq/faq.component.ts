38a1a42f8609e6a806c626e4d8a2adcd1d45ec3e
mbertaina <bertainamalcolm@gmail.com>
2024-12-04 08:33:30 +1100

    feat(BP-1726): rédaction de la partie FAQ

--- src/faq/faq.component.ts
+++ src/faq/faq.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-faq',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, MatExpansionModule],
   templateUrl: './faq.component.html',
-  styleUrl: './faq.component.scss'
+  styleUrl: './faq.component.scss',
 })
 export class FaqComponent {
+  @Input()
+  question!: string;
 
+  @Input()
+  reponse!: string;
 }
