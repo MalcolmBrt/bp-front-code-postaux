34a5884c7062a46fbaea7049cf8f0b7620ede7d1
mbertaina <bertainamalcolm@gmail.com>
2024-12-06 10:33:48 +1100

    feat(BP-1731): versionnge de la partie boites postales

--- src/v2/v2.component.ts
+++ src/v2/v2.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatRadioModule } from '@angular/material/radio';
 import { CodesPostauxComponent } from '../codes-postaux/v2/codes-postaux.component';
-import { BoitesPostalesComponent } from '../boites-postales/v1/boites-postales.component';
+import { BoitesPostalesComponent } from '../boites-postales/v2/boites-postales.component';
+
 @Component({
   selector: 'app-v2',
   standalone: true,
