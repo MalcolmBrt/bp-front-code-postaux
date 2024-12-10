2dd1b8f7c6db114f09e7b349bbb5f8eb88759381
mbertaina <bertainamalcolm@gmail.com>
2024-12-11 08:41:31 +1100

    feat(BP-1733): ajout toggle thème

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent, RouterOutlet],
+  imports: [NavbarComponent, RouterOutlet, MatIconModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'bp-front-code-postaux';
-  choixRecherche = 'CodePostal';
+  isDarkMode = localStorage.getItem('darkmode') === 'active';
+
+  ngOnInit(): void {
+    this.isDarkMode ? this.enableDarkMode() : this.disableDarkMode();
+  }
+
+  disableDarkMode(): void {
+    document.body.classList.remove('darkmode');
+    localStorage.removeItem('darkmode');
+  }
+
+  enableDarkMode(): void {
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkmode', 'active');
+  }
+
+  changeTheme(): void {
+    this.isDarkMode ? this.disableDarkMode() : this.enableDarkMode();
+    this.isDarkMode = !this.isDarkMode;
+  }
 }
