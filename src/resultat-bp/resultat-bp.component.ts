b4d5e54be6c76afd98971b104818669f83e333e2
mbertaina <bertainamalcolm@gmail.com>
2024-12-12 16:24:33 +1100

    feat(BP-1743): ajout d'un bouton pour copier les infos d'une bp

--- src/resultat-bp/resultat-bp.component.ts
+++ src/resultat-bp/resultat-bp.component.ts
@@ -1,9 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { Clipboard } from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'app-resultat-bp',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './resultat-bp.component.html',
   styleUrl: './resultat-bp.component.scss',
 })
@@ -12,4 +15,10 @@ export class ResultatBpComponent {
   @Input() numeroBP!: number;
   @Input() codePostal!: number;
   @Input() localite!: string;
+
+  private clipboard = inject(Clipboard)
+  
+  copier(infos: HTMLElement): void {
+    this.clipboard.copy(infos.innerText)
+  }
 }
