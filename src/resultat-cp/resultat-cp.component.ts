8e546a62ded5c47a51f0710034b368c24c4fb278
mbertaina <bertainamalcolm@gmail.com>
2024-12-20 08:30:09 +1100

    feat(BP-1748): ajout type codes postaux

--- src/resultat-cp/resultat-cp.component.ts
+++ src/resultat-cp/resultat-cp.component.ts
@@ -1,13 +1,14 @@
 import { Component, Input } from '@angular/core';
+import { CodePostal } from '../codes-postaux/v3/code-postal';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-resultat-cp',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './resultat-cp.component.html',
   styleUrl: './resultat-cp.component.scss',
 })
 export class ResultatCpComponent {
-  @Input() localite!: string;
-  @Input() code!: number;
+  @Input() codePostal!: CodePostal;
 }
