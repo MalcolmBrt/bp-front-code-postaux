eba4c2ca2ed59cac3d2f73e274bb7a69b37f2665
mbertaina <bertainamalcolm@gmail.com>
2024-12-20 09:24:20 +1100

    feat(BP-1748): modification des mocks pour ajouter les attributs de typeBp et l'intervalle

--- src/codes-postaux/v3/code-postal.ts
+++ src/codes-postaux/v3/code-postal.ts
@@ -2,7 +2,7 @@ export interface CodePostal {
   id: number;
   localite: string;
   code: number;
-  typeDomicile: true;
+  typeDomicile: boolean;
   typeBp: boolean;
   bpMin: string | null;
   bpMax: string | null;
