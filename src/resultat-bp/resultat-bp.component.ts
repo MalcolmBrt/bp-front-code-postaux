278a9fff04da3dd9faf47795ed99d39e39d6b30f
mbertaina <bertainamalcolm@gmail.com>
2024-12-23 12:43:42 +1100

    feat(BP-1751): réduction taille bouton copier et suppresion texte sur mobile

--- src/resultat-bp/resultat-bp.component.ts
+++ src/resultat-bp/resultat-bp.component.ts
@@ -2,11 +2,14 @@ import { Component, inject, Input, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { Clipboard } from '@angular/cdk/clipboard';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-resultat-bp',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule],
+  imports: [MatButtonModule, MatIconModule, CommonModule],
   templateUrl: './resultat-bp.component.html',
   styleUrl: './resultat-bp.component.scss',
 })
@@ -16,10 +19,17 @@ export class ResultatBpComponent implements OnInit {
   @Input() codePostal!: number;
   @Input() localite!: string;
 
+  isMobile = false;
+
   private clipboard = inject(Clipboard);
+  private breakpointObserver = inject(BreakpointObserver);
 
   ngOnInit(): void {
-    this.numeroBP = this.numeroBP.replace(/^0+/, '');
+    this.breakpointObserver
+      .observe([Breakpoints.Handset])
+      .subscribe((result) => {
+        this.isMobile = result.matches;
+      });
   }
 
   copier(infos: HTMLElement): void {
